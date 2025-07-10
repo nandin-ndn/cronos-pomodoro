c2f2ccc9e9a2b270b744be06d6db0fc31a368a94
Fernando de jesus <fernandojesus934143@gmail.com>
2025-07-10 14:57:47 -0300

    aula 15 props e components

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export function App() {
  console.log('oi');
   return (
   <>
-  <Heading/>
+  <Heading attr={123} attr2= 'string'>Ola mundo 2</Heading>
   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi corporis adipisci praesentium at totam delectus, perspiciatis nihil fugiat accusantium explicabo repellendus laudantium reiciendis eos, assumenda necessitatibus! Vitae corrupti doloremque quae?</p>
   </>
   )
