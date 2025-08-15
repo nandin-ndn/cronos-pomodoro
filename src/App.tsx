0e45421b25a16faabe336c85d351655a843f90ca
Fernando de jesus <fernandojesus934143@gmail.com>
2025-08-14 23:19:45 -0300

    Criando o componente Cycles com indicadores do ciclo pomodoro em cores

--- src/App.tsx
+++ src/App.tsx
@@ -25,17 +25,19 @@ export function App() {
       <Container>
         <form className='form' action=''>
           <div className='formRow'>
-           <DefaultInput labelText= 'task' id='meuInput' type='text' />
+            <DefaultInput
+              labelText='task'
+              id='meuInput'
+              type='text'
+              placeholder='Digite Algo'
+            />
           </div>
 
           <div className='formRow'>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
 
-          <div className='formRow'>
-            <p>Ciclos</p>
-            <p>0 0 0 0 0 0 0</p>
-          </div>
+          <div className='formRow'></div>
 
           <div className='formRow'>
             <button>Enviar</button>
