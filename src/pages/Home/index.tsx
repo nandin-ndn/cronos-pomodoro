98bd7fcf6b86b0f501ea7dcce4b2f814fe2d80c6
Fernando de jesus <fernandojesus934143@gmail.com>
2025-08-20 21:49:40 -0300

    Componente GenericHtml para páginas que renderizam HTML

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,21 @@
+import { Container } from "../../components/Container";
+import { CountDown } from "../../components/CountDown";
+import { Mainform } from "../../components/MainForm";
+import { MainTemplate } from "../../templates/MainTemplate";
+
+
+export function Home(){
+    return (
+    <>
+     <MainTemplate>
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <Mainform />
+      </Container>
+     </MainTemplate>
+    </>
+  );
+}
