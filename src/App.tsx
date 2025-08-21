025493e9ec99012e4fe5f4c0f018e609f99e2764
Fernando de jesus <fernandojesus934143@gmail.com>
2025-08-21 20:39:40 -0300

    Estado global da aplicação com useState e comunicação via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import type { TaskStateModel } from './models/TaskStatemodel';
+import { useState } from 'react';
 import { Home } from './pages/Home';
 
 import './styles/global.css';
 import './styles/theme.css';
 
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  curremtCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
+
 export function App() {
-  return <Home />;
+  const [state, setState] = useState(initialState);
+
+  return <Home state={state} setState={setState} />;
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,27 @@
-import { Container } from "../../components/Container";
-import { CountDown } from "../../components/CountDown";
-import { Mainform } from "../../components/MainForm";
-import { MainTemplate } from "../../templates/MainTemplate";
+import type { TaskStateModel } from '../../models/TaskStatemodel';
+import { Container } from '../../components/Container';
+import { CountDown } from '../../components/CountDown';
+import { Mainform } from '../../components/MainForm';
+import { MainTemplate } from '../../templates/MainTemplate';
 
-
-export function Home(){
-    return (
+type HomeProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+export function Home(props: HomeProps) 
+{
+  const { state, setState } = props;
+  return (
     <>
-     <MainTemplate>
-      <Container>
-        <CountDown />
-      </Container>
+      <MainTemplate>
+        <Container>
+          <CountDown />
+        </Container>
 
-      <Container>
-        <Mainform />
-      </Container>
-     </MainTemplate>
+        <Container>
+          <Mainform />
+        </Container>
+      </MainTemplate>
     </>
   );
 }
